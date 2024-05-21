9ddf61e5683833be4a0cad1274fad19b84e41b07
nahtanpng <nathan.ferreiira13@gmail.com>
2024-05-21 11:34:01 -0300

    feat: :tada: Initial Commit

---
+++ server.js
@@ -0,0 +1,21 @@
+import http from "http";
+
+const PORT = 8080;
+
+//Definindo Rotas
+const rotas = {
+  "/": "Curso de API",
+  "/livros": "Lista de Livros",
+  "/autores": "Lista de Autores",
+  "/hello": "Hello World!",
+};
+
+//Criando um servidor e definindo seu Header e Mensagem
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end(rotas[req.url]);
+});
+
+server.listen(PORT, () => {
+  console.log("Server running at http://127.0.0.1:8080/");
+});
