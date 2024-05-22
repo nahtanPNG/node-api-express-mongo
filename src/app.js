15c5f14083f781fa1809ef141a0496af474acd94
nahtanpng <nathan.ferreiira13@gmail.com>
2024-05-22 14:32:08 -0300

    feat(Livros): :sparkles: CRUD

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 
 const app = express();
+app.use(express.json()); //Middleware -> Acesso as requisiçoes e respostas e converte para JSON
 
 const livros = [
   {
@@ -13,12 +14,40 @@ const livros = [
   },
 ];
 
+function buscaLivro(id) {
+  return livros.findIndex((livro) => {
+    return livro.id === Number(id);
+  });
+}
+
 app.get("/", (req, res) => {
   res.status(200).send("Hello World");
 });
 
 app.get("/livros", (req, res) => {
   res.status(200).json(livros);
+});
+
+app.get("/livros/:id", (req, res) => {
+  const index = buscaLivro(req.params.id);
+  res.status(200).json(livros[index]);
+});
+
+app.post("/livros", (req, res) => {
+  livros.push(req.body);
+  res.status(201).json("Livro cadastrado com sucesso!");
+});
+
+app.put("/livros/:id", (req, res) => {
+  const index = buscaLivro(req.params.id);
+  livros[index].titulo = req.body.titulo;
+  res.status(200).json("Livro atualizado com sucesso!");
+});
+
+app.delete("/livros/:id", (req, res) => {
+  const index = buscaLivro(req.params.id);
+  livros.splice(index, 1);
+  res.status(200).json("Livro removido com sucesso!");
 });
 
 export default app;
